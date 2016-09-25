fd93624ed8c354e75ca37d4ff7c947222499b70e
SergeyKrivohatskiy <s.krivohatskiy@gmail.com>
2016-09-25 03:01:25 +0300

    Annealer class first version interface is written
    Signed-off-by: SergeyKrivohatskiy <s.krivohatskiy@gmail.com>

--- simanneal_cpp/Annealer.cpp
+++ simanneal_cpp/Annealer.cpp
@@ -1,1 +1,48 @@
 #include "Annealer.hpp"
+
+namespace simanneal_cpp
+{
+
+    Annealer::Annealer(state_t && initialState,
+        energy_t initialStateEnergy, std::ostream & updatesOut):
+        m_updatesOut(updatesOut),
+        m_bestState(std::move(initialState)),
+        m_bestStateEnergy(initialStateEnergy)
+    {
+        throw std::runtime_error("Not implemented");
+    }
+
+    void Annealer::runAnnealing(
+        run_schedule const &schedule, size_t updates)
+    {
+        return runAnnealing(schedule.maxT, schedule.minT, schedule.steps, updates);
+    }
+
+    void Annealer::runAnnealing(
+        temperature_t maxT, temperature_t minT, size_t steps, size_t updates)
+    {
+        throw std::runtime_error("Not implemented");
+    }
+
+    Annealer::run_schedule Annealer::computeRunSchedule(
+            double targetRunTime, size_t steps)
+    {
+        throw std::runtime_error("Not implemented");
+    }
+
+    Annealer::state_t const &Annealer::bestState() const
+    {
+        return m_bestState;
+    }
+
+    Annealer::energy_t const &Annealer::bestEnergy() const
+    {
+        return m_bestStateEnergy;
+    }
+
+    void Annealer::printUpdate()
+    {
+        throw std::runtime_error("Not implemented");
+    }
+
+} // namespace simanneal_cpp
--- simanneal_cpp/Annealer.hpp
+++ simanneal_cpp/Annealer.hpp
@@ -1,11 +1,56 @@
 #pragma once
+#include <iostream>
 
 namespace simanneal_cpp
 {
 
+    // TODO rewrite to template<T>, T is state_t
     class Annealer
     {
-    // TODO implementation
+    public:
+        typedef double state_t;
+        typedef double temperature_t;
+        typedef double energy_t;
+        struct run_schedule
+        {
+            temperature_t maxT;
+            temperature_t minT;
+            size_t steps;
+        };
+
+    public:
+        Annealer(state_t &&initialState,
+                 energy_t initialStateEnergy,
+                 std::ostream &updatesOut = std::cout);
+
+        void runAnnealing(
+            run_schedule const &schedule,
+            size_t updates = 100);
+
+        void runAnnealing(
+            temperature_t maxT = 25000.0,
+            temperature_t minT = 2.5,
+            size_t steps = 50000,
+            size_t updates = 100);
+
+        run_schedule computeRunSchedule(
+            double targetRunTime,
+            size_t steps = 2000);
+
+        state_t const &bestState() const;
+
+        energy_t const &bestEnergy() const;
+
+    protected:
+        virtual energy_t moveState(state_t &initialState) const = 0;
+
+    private:
+        void printUpdate();
+
+    private:
+        std::ostream &m_updatesOut;
+        state_t m_bestState;
+        energy_t m_bestStateEnergy;
 	};
 
 } // namespace simanneal_cpp
